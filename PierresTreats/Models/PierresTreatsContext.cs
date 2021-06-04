c603de528bef9b8305db0da8969ce81f444f7af5
Usarneme <jamestcoop@gmail.com>
2021-06-04 08:47:58 -0700

    :card_file_box: adds a database migrations folder and initial migration

--- PierresTreats/Models/PierresTreatsContext.cs
+++ PierresTreats/Models/PierresTreatsContext.cs
@@ -6,6 +6,9 @@ namespace PierresTreats.Models
   public class PierresTreatsContext : IdentityDbContext<ApplicationUser>
   {
     public PierresTreatsContext(DbContextOptions options) : base(options) {}
+    public virtual DbSet<Flavor> Flavors { get; set; }
+    public virtual DbSet<Treat> Treats { get; set; }
+    public DbSet<FlavorTreat> FlavorTreats { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
       optionsBuilder.UseLazyLoadingProxies();
